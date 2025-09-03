91988024ec98cb9a26bfde1ce9ba615a4bba1567
makiandrich <58576784+makiandrich@users.noreply.github.com>
2025-09-03 14:31:03 -0600

    Another Print statement to test

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     //method that prints out hello world
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        System.out.println("Goodbye World");
     }
 }
