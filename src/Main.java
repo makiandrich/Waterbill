4913264bcd3d1f0fbb454782107a6ad2afdbc19e
makiandrich <58576784+makiandrich@users.noreply.github.com>
2025-09-03 14:50:35 -0600

    Added the IF Statement

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,43 @@
 public class Main {
     //method that prints out hello world
     public static void main(String[] args){
-        System.out.println("Hello World");
+        //find the names house type and gallons used
 
-        System.out.println("Goodbye World");
+        String name = "Alice";
+        int gallonsUsed = 8000;
+        int customerType = 1; //for the house type 1= single family and 2 is duplex
+        double bill = 0;
+
+
+        if (customerType == 1) {
+            if (gallonsUsed <= 7000) {
+                bill = 13.21 + gallonsUsed * (2.04 / 1000.0);
+            } else if (gallonsUsed <= 13000) {
+                bill = 13.21 + 7000 * (2.04 / 1000.0)
+                        + (gallonsUsed - 7000) * (2.35 / 1000.0);
+            } else {
+                bill = 13.21 + 7000 * (2.04 / 1000.0)
+                        + 6000 * (2.35 / 1000.0)
+                        + (gallonsUsed - 13000) * (2.70 / 1000.0);
+            }
+        } else {
+            if (gallonsUsed <= 7000) {
+                bill = 15.51 + gallonsUsed * (1.97 / 1000.0);
+            } else if (gallonsUsed <= 13000) {
+                bill = 15.51 + 9000 * (1.97 / 1000.0)
+                        + (gallonsUsed - 9000) * (2.26 / 1000.0);
+            } else {
+                bill = 15.51 + 9000 * (1.97 / 1000.0)
+                        + 4000 * (2.26 / 1000.0)
+                        + (gallonsUsed - 13000) * (2.60 / 1000.0);
+            }
+        }
+
+        System.out.println("Customer Type: " + customerType);
+        System.out.println("Gallons Used: " + gallonsUsed);
+        System.out.println("Custoemr Name: " + name);
+        System.out.println("Bill: " + bill);
+
+
     }
 }
